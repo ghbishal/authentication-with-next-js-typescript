0bbe2ede5efa001d8320fa8717528250cc12540c
ghbishal <bishalk2056@gmail.com>
2023-09-06 00:12:07 +0900

    feat: nextauthsetup & github login

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID as string,
+      clientSecret: process.env.FACEBOOK_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import { NextPageContext } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <>
       <h1>Welocome </h1>
+      <button onClick={() => signOut()}>Sign out</button>
     </>
   );
+}
+
+export async function getServerSideProps(ctx: NextPageContext) {
+  const session = await getSession(ctx);
+  console.log(session);
+  return {
+    props: {
+      session,
+    },
+  };
 }
