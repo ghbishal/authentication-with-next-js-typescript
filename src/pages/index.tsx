e10d7c45f6b37ba3b58a11e17c39ec590f8f3a63
ghbishal <bishalk2056@gmail.com>
2023-09-06 11:22:03 +0900

    feat: googleProvider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,20 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        <h1 className="bg-orange-300 w-full">{session.user?.name}</h1>
+        <button onClick={() => signIn()}>Sing Out</button>
+      </>
+    );
+  }
+
   return (
     <>
       <h1>Welocome </h1>
-      <button onClick={() => signOut()}>Sign out</button>
+      <button onClick={() => signIn()}>Sign In</button>
     </>
   );
 }
