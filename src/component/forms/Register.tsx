b3c7a5f41b5adc24806859870b945561245aa4a6
ghbishal <bishalk2056@gmail.com>
2023-09-07 05:27:29 +0900

    feat: add zod validation

--- src/component/forms/Register.tsx
+++ src/component/forms/Register.tsx
@@ -2,8 +2,20 @@ import * as React from "react";
 import Input from "@/component/inputs/Input";
 import { CiUser } from "react-icons/ci";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 type Props = {};
+
+const FormSchema = z.object({
+  first_name: z
+    .string()
+    .min(2, "First name must be atleast 2 characters")
+    .max(32, "First name must be less than 32 characters")
+    .regex(new RegExp("[a-zA-Z]+$"), "No special characters allowed"),
+});
+
+type FormSchemaType = z.infer<typeof FormSchema>;
 
 export default function Register(props: Props) {
   const {
@@ -11,10 +23,11 @@ export default function Register(props: Props) {
     handleSubmit,
     watch,
     formState: { errors, isSubmitting },
-  } = useForm();
+  } = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
+  });
 
   const onSubmit = (data: any) => console.log(data);
-  console.log(watch);
 
   return (
     <form className="my-8 text-sm" onSubmit={handleSubmit(onSubmit)}>
@@ -29,6 +42,7 @@ export default function Register(props: Props) {
           error={errors?.first_name?.message}
           disabled={isSubmitting}
         />
+        <button type="submit">submit</button>
       </div>
     </form>
   );
