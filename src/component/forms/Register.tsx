7567d048d561577180d5d2547d7c1144bc8753dd
ghbishal <bishalk2056@gmail.com>
2023-09-07 05:16:50 +0900

    feat: react hook form for Input

--- src/component/forms/Register.tsx
+++ src/component/forms/Register.tsx
@@ -1,19 +1,33 @@
 import * as React from "react";
 import Input from "@/component/inputs/Input";
 import { CiUser } from "react-icons/ci";
+import { useForm } from "react-hook-form";
 
 type Props = {};
 
 export default function Register(props: Props) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = (data: any) => console.log(data);
+  console.log(watch);
+
   return (
-    <form className="my-8 text-sm">
+    <form className="my-8 text-sm" onSubmit={handleSubmit(onSubmit)}>
       <div className="gap-2 md:flex">
         <Input
           name="first_name"
-          label="First_name"
+          label="First name"
           type="text"
           icon={<CiUser />}
           placeholder="example"
+          register={register}
+          error={errors?.first_name?.message}
+          disabled={isSubmitting}
         />
       </div>
     </form>
--- src/component/inputs/Input.tsx
+++ src/component/inputs/Input.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IoAlertCircle } from "react-icons/io5";
 
 type Props = {
   name: string;
@@ -6,19 +7,49 @@ type Props = {
   type: string;
   icon: JSX.Element;
   placeholder: string;
+  register: any;
+  error: any;
+  disabled: boolean;
 };
 
-export default function Input({ name, label, type, icon, placeholder }: Props) {
+export default function Input({
+  name,
+  label,
+  type,
+  icon,
+  placeholder,
+  register,
+  error,
+  disabled,
+}: Props) {
   return (
-    <div className="relative rounded-md shadow-sm mt-1">
-      <div className="pointer-event-none absolute left-0 top-0.5 inset-y-0 flex items-center pl-3">
-        <span className="text-gray-500 text-sm">{icon}</span>
+    <div className="mt-3 w-[100%]">
+      <label htmlFor={name} className="text-gray-700">
+        {label}
+      </label>
+      <div className="relative rounded-md mt-1">
+        <div
+          className="pointer-event-none absolute left-0 top-0.5 inset-y-0 flex items-center pl-3"
+          style={{ transform: `${error ? "translateY(-10px)" : ""}` }}
+        >
+          <span className="text-gray-500 text-sm">{icon}</span>
+        </div>
+        <input
+          type={type}
+          placeholder={placeholder}
+          className="w-full py-2 pr-7 pl-8 block rounded-md border border-gray-300 outline-offset-2 outline-transparent focus:border-blue-500 focus:ring-blue-700 focus:ring-2 text-sm"
+          {...register(name)}
+          style={{
+            borderColor: `${error ? "#ED4337" : ""}`,
+          }}
+        />
+        {error && (
+          <div className="fill-red-500 absolute right-2 top-2.5 text-xl">
+            <IoAlertCircle fill="#ED4337" />
+          </div>
+        )}
+        {error && <p className="text-sm text-[#ED4337] mt-1">{error}</p>}
       </div>
-      <input
-        type={type}
-        placeholder={placeholder}
-        className="w-full py-2 pr-7 pl-8 block rounded-md border border-gray-300 outline-offset-2 outline-transparent focus:border-blue-500 focus:ring-blue-700 focus:ring-2 text-sm"
-      />
     </div>
   );
 }
